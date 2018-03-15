eebe91a02d3e05628c191e5ff0682d31147f06dc
Lammiman <alammiman@Companynet.org>
2018-03-15 13:01:10 +0000

    Added gulp file with watch and local dev server set up.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,58 @@
 "use strict"
 
 var gulp = require('gulp');
-var connect = require('gulp-connect');
-var open = require('gulp-open');
+var connect = require('gulp-connect'); //local dev server
+var open = require('gulp-open'); //opens a url in web browser
+var browserify = require('browserify'); //js bundle
+var reactify = require('reactify'); //transforms react to js
+var source = require('vinyl-source-stream'); //use convential text streams with Gulp
 
-gilp.task('connect', function() {
+var config = {
+    port: 9005,
+    devBaseUrl: 'http://localhost',
+    paths: {
+        html: './src/*.html',
+        js: './src/**/*.js',
+        dist: './dist',
+        mainJs: './src/main.js'
+    }
+} 
+
+//start local server
+gulp.task('connect', function() {
         connect.server({
             root: ['dist'],
-            port: config.port    
-
+            port: config.port,
+            base: config.devBaseUrl,    
+            livereload: true 
         });
 
 })
+
+gulp.task('open', ['connect'], function() {
+    gulp.src('dist/index.html')
+        .pipe(open({uri: config.devBaseUrl + ':' + config.port + '/'}));
+});
+
+gulp.task('html', function(){
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('js', function(){
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function(){
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
+});
+
+gulp.task('default',['html', 'js', 'open', 'watch']);
---
+++ src/main.js
@@ -0,0 +1,3 @@
+var App = console.log('foo');
+
+module.exports = App;
