ecf359cae88756b71002b15f2909b03097cea8ab
ajlammiman <alammiman@Companynet.org>
2018-04-19 12:30:39 +0100

    Updated checkoutproject powershell

--- checkoutproject.ps1
+++ checkoutproject.ps1
@@ -1,6 +1,25 @@
-﻿function SetUpNewReactProject($folderName) 
+﻿$repoPath = https://github.com/ajlammiman/
+
+
+function SetUpNewReactProject($folderName) 
 {
     git clone https://github.com/ajlammiman/reactprojectbase.git C:\inetpub\wwwroot\$folderName
 }
 
+
+function SetUpNewConsoleProject($folderName) 
+{
+   $repo = $repoPath + '/basicconsoleproject.git'
+   $path = C:\inetpub\wwwroot\$folderName
+
+    GitCheckoutRepo($repo, $path)
+}
+
+function GitCheckoutRepo($repo, $path)
+{
+    git clone $repo $path
+}
+
+
+SetUpNewConsoleProject consoleTest
 
